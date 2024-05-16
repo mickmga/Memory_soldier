75d8e2c8c9f92227bf8a63f77d20958836e2f443
Ludovicmanga <ludovic.mangaj@gmail.com>
2024-05-16 19:49:16 +0200

    camera  movement ok

--- frontend/palace.js
+++ frontend/palace.js
@@ -1,3 +1,27 @@
 window.onload = function () {
     console.log("let's go, you are ready to start building");
 };
+let launched = false;
+
+
+document.addEventListener("keydown", function (event) {
+    
+    if (event.key === 'd' && launched === false) {
+        cameraMovement();
+    }
+});
+
+
+const cameraMovement = () => {
+
+  launched = true;
+
+  const gameContainer = document.getElementById("gameContainer");
+
+  const gameContainerLeft = gameContainer.offsetLeft;
+
+  gameContainer.style.left = `${gameContainerLeft-3}px`;
+
+  requestAnimationFrame(cameraMovement);
+
+}
--- frontend/palace.ts
+++ frontend/palace.ts
@@ -1,4 +1,12 @@
 window.onload = () => {
  console.log("let's go, you are ready to start building");
 }
+
+document.addEventListener("keydown", (event) => {
+
+    if(event.code === 'd'){
+        alert("moving cam");
+    }
+
+})
   
